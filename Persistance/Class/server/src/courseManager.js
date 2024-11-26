e6e611f6df0326fc1d132431c8e3e9978a8fec67
LpCote <loulou4@live.ca>
2024-11-26 08:42:27 -0500

    exo persistance

--- Persistance/Class/server/src/courseManager.js
+++ Persistance/Class/server/src/courseManager.js
@@ -1,77 +1,112 @@
 const fsPromises = require('fs').promises;
 const path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const DB_CONSTS = require("./env");
 
 class CourseManager {
     constructor() {
-        this.courses = [];
-        this.path = path.join(__dirname + "../../data/courses.json");
+        // this.courses = [];
+        // this.path = path.join(__dirname + "../../data/courses.json");
+        this.collection = null;
+        this.client = null;
+        this.options = {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        };
     }
 
     async init() {
         try {
-            const courses = await fsPromises.readFile(this.path);
-            this.courses = JSON.parse(courses);
+            this.client = await MongoClient.connect(DB_CONSTS.DB_URL, this.options);
+            this.collection = this.client.db(DB_CONSTS.DB_DB).collection(DB_CONSTS.DB_COLLECTION);
+            this.collection.createIndex("sigle", { unique: true });
+            console.log("Connexion à la base de données réussie");
+            // const courses = await fsPromises.readFile(this.path);
+            // this.courses = JSON.parse(courses);
         }
         catch (e) {
-            console.log(`Aucun fichier trouvé. Chargement d'une liste vide`);
-            this.courses = [];
+            console.error(e);
+            // console.log(`Aucun fichier trouvé. Chargement d'une liste vide`);
+            // this.courses = [];
         }
-        finally {
-            return this.courses;
-        }
-    }
-
-    async saveCoursesToFile() {
-        await fsPromises.writeFile(this.path, JSON.stringify(this.courses, null, 4));
+        // finally {
+        //     return this.courses;
+        // }
     }
 
     async getAllCourses() {
-        return this.courses;
+        return this.collection.find({}, { projection: { _id: 0 } }).toArray();
     }
 
-    findCourse(sigle) {
-        return this.courses.find((c) => {
-            return c.sigle === sigle;
-        });
+    findCourse(sigle) { 
+        return this.collection.findOne({ sigle }, { projection: { _id: 0 } });
+        // return this.courses.find((c) => {
+        //     return c.sigle === sigle;
+        // });
     }
 
     async addCourse(sigle) {
         const course = { sigle, credits: Math.floor(Math.random() * 5 + 1) };
-        if (this.courses.find(x => x.sigle === course.sigle)) {
+        try {
+            await this.collection.insertOne(course);
+            return true;
+        }
+        catch (e) {
+            console.error(e);
             return false;
         }
-        this.courses.push(course);
-        await this.saveCoursesToFile();
-        return true;
+        // if (this.courses.find(x => x.sigle === course.sigle)) {
+        //     return false;
+        // }
+        // this.courses.push(course);
+        // await this.saveCoursesToFile();
+        // return true;
     }
 
     async deleteCourse(sigle) {
-        const size = this.courses.length;
-        this.courses = this.courses.filter((c) => c.sigle !== sigle);
-        await this.saveCoursesToFile();
-        return size > this.courses.length;
+        const deleteResult = await this.collection.deleteMany({ sigle });
+        return deleteResult.deletedCount;
+        // const size = this.courses.length;
+        // this.courses = this.courses.filter((c) => c.sigle !== sigle);
+        // await this.saveCoursesToFile();
+        // return size > this.courses.length;
     }
 
     async modifyCourse(modifiedCourse) {
-        const course = this.courses.find((c) => {
-            return c.sigle === modifiedCourse.sigle;
-        });
-        if (!course) return false;
-        course.credits = modifiedCourse.credits;
-        await this.saveCoursesToFile();
-        return true;
+        const modifiedResult = await this.collection.updateOne(
+            { sigle: modifiedCourse.sigle },
+            { $set: { credits: modifiedCourse.credits } });
+        return modifiedResult.matchedCount;
+        // const course = this.courses.find((c) => {
+        //     return c.sigle === modifiedCourse.sigle;
+        // });
+        // if (!course) return false;
+        // course.credits = modifiedCourse.credits;
+        // await this.saveCoursesToFile();
+        // return true;
     }
 
     async deleteAll() {
         try {
-            await fsPromises.unlink(this.path);
-            this.courses = [];
+            await this.collection.deleteMany({});
             return true;
         }
         catch (e) {
             console.log('Erreur de suppression');
             return false;
         }
+        // try {
+        //     await fsPromises.unlink(this.path);
+        //     this.courses = [];
+        //     return true;
+        // }
+        // catch (e) {
+        //     console.log('Erreur de suppression');
+        //     return false;
+        // }
     }
 
 }
