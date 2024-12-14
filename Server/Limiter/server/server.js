325fef46372b5a792f8f3d2abc29c489e6b108c2
LouisPCote <louis-philippe.cote@polymtl.ca>
2024-12-13 22:01:51 -0500

    Done: exo server/limiter

--- Server/Limiter/server/server.js
+++ Server/Limiter/server/server.js
@@ -26,6 +26,18 @@ app.get('/token', (req, res) => {
     res.send({ token, tokenCount: limits.size });
 });
 
+// TODO : remettre la limite du jeton à MAX_REQUESTS si le jeton existe
+app.patch("/token/reset", (req, res) => {
+    const token = req.header('X-AUTH');
+    if(limits.has(token)) {
+        limits.set(token, { count: MAX_REQUESTS });
+        res.set("X-LIMIT", MAX_REQUESTS);
+        res.status(204).send();
+        return;
+    }
+    res.status(404).send("Ce jeton n'existe pas");
+});
+
 /* TODO: Implémenter la récupération des livres 
     avec une gestion des requêtes permises.
     Une requête GET vers /books?maxCount={true|false} devrait retourner tous les livres
@@ -37,12 +49,47 @@ app.get('/token', (req, res) => {
     Chaque requête acceptée diminue le compteur pour ce jeton de 1
     Si le compteur est = 0, la réponse est 429 Too Many Requests
 */
-
-
-// TODO : remettre la limite du jeton à MAX_REQUESTS si le jeton existe
-app.patch("/token/reset", (req, res) => {
-    res.status(404).send("Ce jeton n'existe pas");
+app.use('/books', (req, res, next) => {
+    const token = req.header('X-AUTH');
+    if (!token || !limits.has(token)) {
+        res.status(403).send("Forbidden");
+        return;
+    }
+    const limit = limits.get(token);
+    if (limit.count <= 0) {
+        res.status(429).send("Too Many Requests");
+        return;
+    }
+    limit.count--;
+    res.set("X-LIMIT", limit.count);
+    next();
 });
+
+app.get('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const random = req.query.random;
+    if(random === 'true') {
+        const randomIndex = Math.floor(Math.random() * books.length);
+        res.send(books[randomIndex]);
+    } else {
+        const book = books.find(book => book.id === bookId);
+        if (!book) {
+            res.status(404).send("Book not found");
+            return;
+        }
+        res.send(book);
+    }
+});
+
+app.get('/books', (req, res) => {
+    let maxCount = parseInt(req.query.maxCount);
+    if(!maxCount || maxCount < 0 || maxCount > books.length) {
+        maxCount = books.length;
+    }
+    const booksToSend = books.slice(0, maxCount);
+    res.send(booksToSend);
+})
+
 
 app.delete('/reset', (req, res) => {
     limits.clear();
