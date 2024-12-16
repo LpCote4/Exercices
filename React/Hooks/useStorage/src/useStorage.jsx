b797420653c610ae13292ce66ea0e498f35010ae
LouisPCote <louis-philippe.cote@polymtl.ca>
2024-12-16 10:15:03 -0500

    done: useStorage exo

--- React/Hooks/useStorage/src/useStorage.jsx
+++ React/Hooks/useStorage/src/useStorage.jsx
@@ -1,4 +1,25 @@
+import {useEffect, useState} from "react"
+
 
 export default function useStorage(key, defaultData, isSession = false) {
-    return ['', () => { }];
+    // return ['', () => { }];
+    const storage = isSession ? sessionStorage : localStorage;
+    const [data, setData] = useState(
+        JSON.parse(storage.getItem(key)) || String(defaultData)
+    );
+
+    useEffect(() => {
+        storage.setItem(key, JSON.stringify(data));
+    }, [data, key]);
+
+    //vider le storage
+    useEffect(() => {
+        window.addEventListener('storage', (e) => {
+            if(e.key===null){
+                setData(defaultData);
+            }
+        })
+    })
+
+    return [data, setData];
 };
